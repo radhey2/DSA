7481151d0ac21f2c14801c4176991567a498a7bd
radhey2 <96431541+radhey2@users.noreply.github.com>
2022-03-08 11:23:33 +0530

    DSA Day 16-17

---
+++ AnagramUc6.java
@@ -0,0 +1,45 @@
+package datastructure;
+
+import java.util.Arrays;
+
+public class AnagramUc6 {
+	  /* function to check whether two strings are
+    anagram of each other */
+    static boolean areAnagram(char[] str1, char[] str2)
+    {
+        // Get lengths of both strings
+        int n1 = str1.length;
+        int n2 = str2.length;
+ 
+        // If length of both strings is not same,
+        // then they cannot be anagram
+        if (n1 != n2)
+            return false;
+ 
+        // Sort both strings
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+ 
+        // Compare sorted strings
+        for (int i = 0; i < n1; i++)
+            if (str1[i] != str2[i])
+                return false;
+ 
+        return true;
+    }
+ 
+    /* Driver Code*/
+    public static void main(String args[])
+    {
+        char str1[] = { 'l', 'i', 's', 't','e','n' };
+        char str2[] = { 's', 'i', 'l', 'e','n','t' };
+       
+        // Function Call
+        if (areAnagram(str1, str2))
+            System.out.println("The two strings are"
+                               + " anagram of each other");
+        else
+            System.out.println("The two strings are not"
+                               + " anagram of each other");
+    }
+}
